cd631252b29d22e102199e91cc3bfb84d7ed3775
Nikita Goncharuk <nikitago2000@gmail.com>
2022-04-29 20:46:59 +0300

    Add Play Store Badge to index page

--- packages/webapp/src/pages/Info/index.tsx
+++ packages/webapp/src/pages/Info/index.tsx
@@ -2,10 +2,14 @@ import classes from './Info.module.css';
 
 const Info: React.FC<{}> = props => {
     return <div className={classes.Info}>
-        <p>Want to test the FOMO Nouns mobile apps? Get the instructions in NounsDAO discord:</p>
-        <a href="https://discord.com/channels/849745721544146955/892962966226473100/967151287479185449">Install app on Android</a>
+        <p>Want to test the FOMO Nouns mobile apps?</p>
+        <a href="https://twitter.com/nikitago_/status/1518775085070487552">How to install app on iOS</a>
         <br />
-        <a href="https://discord.com/channels/849745721544146955/892962966226473100/967568231709704222">Install app on iOS</a>
+        <div>
+            <a href='https://play.google.com/store/apps/details?id=com.fomonouns.ng.mobileapp&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+                <img className={classes.badge} alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' />
+            </a>
+        </div>
     </div>
 }
 
