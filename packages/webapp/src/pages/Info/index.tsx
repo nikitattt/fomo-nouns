078942a478bd8c45da8f4959a0a26c3b1d211b25
ng <hey@nikitagoncharuk.com>
2022-05-17 04:54:53 +0300

    Add phone preview to info page

--- packages/webapp/src/pages/Info/index.tsx
+++ packages/webapp/src/pages/Info/index.tsx
@@ -1,16 +1,32 @@
-import classes from './Info.module.css';
+import classes from "./Info.module.css";
 
-const Info: React.FC<{}> = props => {
-    return <div className={classes.Info}>
-        <p>Want to test the FOMO Nouns mobile apps?</p>
-        <a href="https://twitter.com/nikitago_/status/1518775085070487552">How to install app on iOS</a>
-        <br />
-        <div>
-            <a href='https://play.google.com/store/apps/details?id=com.fomonouns.ng.mobileapp&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
-                <img className={classes.badge} alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' />
-            </a>
-        </div>
+import phonePreview from "../../assets/iphone-preview.png";
+
+const Info: React.FC<{}> = (props) => {
+  return (
+    <div className={classes.Info}>
+      <p className={classes.title}>Want to test the FOMO Nouns mobile apps?</p>
+      <div>
+        <a href="https://play.google.com/store/apps/details?id=com.fomonouns.ng.mobileapp&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
+          <img
+            className={classes.badge}
+            alt="Get it on Google Play"
+            src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+          />
+        </a>
+      </div>
+      <br />
+      <a href="https://twitter.com/nikitago_/status/1518775085070487552">
+        How to install app on iOS
+      </a>
+      <br />
+      <img
+        className={classes.phone}
+        alt="Fomo Nouns app preview on iPhone"
+        src={phonePreview}
+      />
     </div>
-}
+  );
+};
 
-export default Info
+export default Info;
